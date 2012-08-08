9530d3db2432d31a8c5dae6e0a42a0ee6472e80c
scotty <scotty.cn@gmail.com>
2012-08-08 12:30:31 +0800

    added more to the solution skeleton
    added projects: mvc, service, modeling (for design), and some
    corresponding test projects.

---
+++ Service.Tests/BloggingServiceTest.cs
@@ -0,0 +1,110 @@
+﻿using Service;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting.Web;
+
+namespace Service.Tests
+{
+    
+    
+    /// <summary>
+    ///This is a test class for BloggingServiceTest and is intended
+    ///to contain all BloggingServiceTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class BloggingServiceTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for GetData
+        ///</summary>
+        // TODO: Ensure that the UrlToTest attribute specifies a URL to an ASP.NET page (for example,
+        // http://.../Default.aspx). This is necessary for the unit test to be executed on the web server,
+        // whether you are testing a page, web service, or a WCF service.
+        [TestMethod()]
+        [HostType("ASP.NET")]
+        [AspNetDevelopmentServerHost("D:\\Projects\\MyProjs\\ScottyBlogging\\ScottyBlogging\\Service", "/")]
+        [UrlToTest("http://localhost:65230/")]
+        public void GetDataTest()
+        {
+            BloggingService target = new BloggingService(); // TODO: Initialize to an appropriate value
+            int value = 0; // TODO: Initialize to an appropriate value
+            string expected = string.Empty; // TODO: Initialize to an appropriate value
+            string actual;
+            actual = target.GetData(value);
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+
+        /// <summary>
+        ///A test for GetDataUsingDataContract
+        ///</summary>
+        // TODO: Ensure that the UrlToTest attribute specifies a URL to an ASP.NET page (for example,
+        // http://.../Default.aspx). This is necessary for the unit test to be executed on the web server,
+        // whether you are testing a page, web service, or a WCF service.
+        [TestMethod()]
+        [HostType("ASP.NET")]
+        [AspNetDevelopmentServerHost("D:\\Projects\\MyProjs\\ScottyBlogging\\ScottyBlogging\\Service", "/")]
+        [UrlToTest("http://localhost:65230/")]
+        public void GetDataUsingDataContractTest()
+        {
+            BloggingService target = new BloggingService(); // TODO: Initialize to an appropriate value
+            CompositeType composite = null; // TODO: Initialize to an appropriate value
+            CompositeType expected = null; // TODO: Initialize to an appropriate value
+            CompositeType actual;
+            actual = target.GetDataUsingDataContract(composite);
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+    }
+}
