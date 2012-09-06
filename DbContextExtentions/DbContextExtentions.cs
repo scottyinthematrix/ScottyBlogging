89e3c898d57183f7f7d722ade5fa857c4bbf377a
unknown <scotty.gang.yang@CDC312800.GDNCHINA.COM>
2012-09-06 18:07:46 +0800

    added new project ScottyBloggingResx for sharing resources across multiple projects; extended the DbContext on updating only dirty fields; implementing the logics on Writer (in progress);

---
+++ DbContextExtentions/DbContextExtentions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScottyApps.Utilities.DbContextExtentions
+{
+    public static class DbContextExtentions
+    {
+        public static void Update<TEntry>(this DbContext ctx, TEntry entity, params Expression<Func<TEntry, string>>[] dirtyFields) where TEntry : class
+        {
+            ctx.Set(typeof(TEntry)).Attach(entity);
+
+            if (dirtyFields != null && dirtyFields.Length > 0)
+            {
+                dirtyFields.ToList().ForEach(field => {
+                    ctx.Entry(entity).Property(field).IsModified = true;
+                });
+            }
+            else
+            {
+                ctx.Entry(entity).State = System.Data.EntityState.Modified;
+            }
+
+            ctx.SaveChanges();
+        }
+    }
+}
--- Entity/Writer.cs
+++ Entity/Writer.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
 using System.Runtime.Serialization;
+using System.Security.Cryptography;
+using ScottyApps.ScottyBlogging.Resx;
+using ScottyApps.Utilities.DbContextExtentions;
 
 namespace ScottyApps.ScottyBlogging.Entity
 {
@@ -23,17 +28,73 @@ namespace ScottyApps.ScottyBlogging.Entity
 
         public RegisterStatus Register()
         {
-            throw new NotImplementedException("hurry up, scotty!");
+            using (BloggingContext ctx = new BloggingContext())
+            {
+                var existingWriter = ctx.Writers.Single(w => w.Email == this.Email);
+                if (existingWriter != null)
+                {
+                    return RegisterStatus.UserExisting;
+                }
+
+                if (string.IsNullOrEmpty(this.ID))
+                {
+                    this.ID = Guid.NewGuid().ToString();
+                }
+
+                // TODO encrypt the password
+                ctx.Writers.Add(this);
+                ctx.SaveChanges();
+                return RegisterStatus.OK;
+            }
         }
         public UserValidationStatus Validate()
         {
-            throw new NotImplementedException("hurry up, scotty!");
+            using (BloggingContext ctx = new BloggingContext())
+            {
+                var existingWriter = ctx.Writers.Single(w => w.Email == this.Email);
+                if (existingWriter == null)
+                {
+                    return UserValidationStatus.UserNotExist;
+                }
+
+                // TODO should decrypt the password then do the comparison
+                if (existingWriter.Password == this.Password)
+                {
+                    return UserValidationStatus.OK;
+                }
+                else
+                {
+                    return UserValidationStatus.WrongPassword;
+                }
+            }
         }
         public void Update()
         {
+            if (string.IsNullOrEmpty(this.ID))
+            {
+                throw new InvalidOperationException(ScottyBloggingResx.exMsg_EmptyWriterID);
+            }
+
+            using (BloggingContext ctx = new BloggingContext())
+            {
+                //ctx.Writers.Attach(this);
+                //ctx.Entry<Writer>(this).Property(w => w.Alias).IsModified = true;
+                //ctx.SaveChanges();
+                ctx.Update(this, w => w.Alias);
+            }
         }
         public void Delete()
         {
+            if (string.IsNullOrEmpty(this.ID))
+            {
+                throw new InvalidOperationException(ScottyBloggingResx.exMsg_EmptyWriterID);
+            }
+
+            using (BloggingContext ctx = new BloggingContext())
+            {
+                ctx.Writers.Remove(this);
+                ctx.SaveChanges();
+            }
         }
     }
 }
