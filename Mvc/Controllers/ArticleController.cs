d9cfadf9abe105040c06ad060001c792b2e9a2b6
unknown <scotty.gang.yang@CDC312800.GDNCHINA.COM>
2013-01-04 17:19:01 +0800

    start to work on MVC part (nothing really meaningful right now)

---
+++ Mvc/Controllers/ArticleController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ScottyApps.ScottyBlogging.Entity;
+
+namespace Mvc.Controllers
+{
+    public class ArticleController : Controller
+    {
+        //
+        // GET: /Article/
+
+        public ActionResult Index()
+        {
+            var articles = new List<Article>
+                               {
+                                   new Article{Body = "test", CreateDate = DateTime.Now, ID=Guid.NewGuid().ToString(),Title = "test title"}
+                               };
+            //ViewBag.Articles = articles;
+            return View(articles);
+        }
+
+    }
+}
