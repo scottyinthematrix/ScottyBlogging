cccab6a6950fa9ff63668b1f7c1349bc9dc334b9
unknown <scotty.gang.yang@CDC312800.GDNCHINA.COM>
2013-01-16 18:41:24 +0800

    successfully imported ckeditor for /Article/Edit
    the first time, need more improvements

--- Mvc/Controllers/ArticleController.cs
+++ Mvc/Controllers/ArticleController.cs
@@ -17,12 +17,30 @@ namespace Mvc.Controllers
             return View();
         }
 
-        public ActionResult Get()
+        private ActionResult Get()
         {
             return Json(new { Name = "scotty", Epoch = new DateTime(1970, 1, 1), BirthDate = new DateTime(1981, 7, 10) }, JsonRequestBehavior.AllowGet);
 
             // NOTE - will generate below json string:
             // {"Name":"scotty","BirthDate":"\/Date(363542400000)\/"}
+        }
+
+        public ActionResult Read(string id)
+        {
+            return View();
+        }
+
+        public ActionResult Read2(string title)
+        {
+            return null;
+        }
+
+        [HttpGet]
+        public ActionResult Edit(string id)
+        {
+            ViewBag.ArticleTitle = string.Format("Title of {0}", id);
+            ViewBag.ArticleContent = string.Format("Here goes some content of the article of which the id is {0}", id);
+            return View();
         }
     }
 }
